494b9c9d1c5248537687ec0f464555fd68d76072
CarbondaleEstate <detff3@me.com>
2026-09-07 15:41:55 -0700

    Update service worker version to 0.9

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSION = 'carbondale-phone-link-sw-0.7';
+const VERSION = 'carbondale-phone-link-sw-0.9';
 const DB_NAME = 'carbondale-phone-link';
 const DB_VERSION = 1;
 const STORE = 'state';
